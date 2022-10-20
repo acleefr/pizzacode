4cc45881c19e72cf7c875ae00a38befe36bb872e
Paul Collas <paulcollas446@gmail.com>
2022-10-20 11:23:20 +0200

    Mise en place des components dans un dossier componenents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { HomeComponent } from './pages/home/home.component';
-import { SliderComponent } from './slider/slider.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
